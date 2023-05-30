3d0078f9af6fe5a01b07f43df5aa23827b99ddcb
Stephen Irinoye <123007810+Stephen-I@users.noreply.github.com>
2023-05-30 14:33:58 +0100

    Add files via upload

---
+++ text-adventure-starting/text-adventure.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+int response;
+
+    std::cout << "Your a mercenary on the hunt for the culprit of the caravan robberies. It been a rough day and your having no luck.";
+    std::cout << "Then out of knowhere a bright haired beauty whizzed past and snatched up your tresured necklase and makes a run for it.";
+    std::cout << "What do you do in response?";
+    std::cout << "1. Give chase";
+    std::cout << "2. Throw your weapon";
+    std::cout << "3. Let her go";
+    std::cin >> response;
+
+    if(response = 1){}
+    
+
+
+}
